0a07d8cec9d5f0b8c1ede06a95e2a71387cfb42d
Felipe Staub <lipestaub@gmail.com>
2022-12-01 11:03:13 -0300

    style: ajustar tabela da tela de listar usuarios

--- resources/views/usuarios/listar.blade.php
+++ resources/views/usuarios/listar.blade.php
@@ -5,7 +5,6 @@
     <h3>Listar usuários</h3>
     <br>
 </div>
-<div class="container">
     <div class="row text-center">
         <div class="col-sm-1">
             <b>Nome</b>
@@ -13,7 +12,7 @@
         <div class="col-sm-1">
             <b>Tipo</b>
         </div>
-        <div class="col-sm-1">
+        <div class="col-sm-3">
             <b>Op&ccedil;&otilde;es</b>
         </div>
     </div>
@@ -26,7 +25,7 @@
         <div class="col-sm-1">
             {{ $usuario->user_type }}
         </div>
-        <div class="col-sm-1">
+        <div class="col-sm-3">
             <div class="d-flex">
                 <a class="btn btn-primary" href="/usuarios/editar/{{ $usuario->id }}"><i class="fa fa-pencil-square-o"></i> Editar</a>
                 <a class="btn btn-danger" href="/usuarios/deletar/{{ $usuario->id }}"><i class="fa fa-remove"></i> Deletar</a>
@@ -35,5 +34,4 @@
     </div>
     <br>
     @endforeach
-</div>
 @endsection
