db2db67e4d3ac9e34f6277e5d6ae0665d24af861
Felipe Staub <lipestaub@gmail.com>
2022-12-01 11:30:38 -0300

    style: ajustar tabela do carrinho

--- resources/views/carrinho.blade.php
+++ resources/views/carrinho.blade.php
@@ -13,7 +13,6 @@
     @endif
     
     <br>
-    <div class="container">
         <div class="row text-center">
             <div class="col-sm-1">
                 <b>Descri&ccedil;&atilde;o</b>
@@ -27,7 +26,7 @@
             <div class="col-sm-2">
                 <b>Subtotal</b>
             </div>
-            <div class="col-sm-1">
+            <div class="col-sm-3">
                 <b>Op&ccedil;&otilde;es</b>
             </div>
         </div>
@@ -46,7 +45,7 @@
             <div class="col-sm-2">
                 {{ 'R$ ' . number_format((float) (str_replace(',', '.', $orderedItem->product->price) * $orderedItem->quantity), 2, ',', '.') }}
             </div>
-            <div class="col-sm-1">
+            <div class="col-sm-3">
                 <div class="d-flex">
                     <a class="btn btn-primary" href="/carrinho/editar/{{ $orderedItem->id }}"><i class="fa fa-pencil-square-o"></i> Editar</a>
                     <a class="btn btn-danger" href="/carrinho/deletar/{{ $orderedItem->id }}"><i class="fa fa-remove"></i> Deletar</a>
@@ -56,12 +55,13 @@
         <br>
         @endforeach
     </div>
-    <h5>Valor total: R$ {{ $valorTotalCarrinho }}</h5>
-    {{ Form::open(['url' => 'carrinho/finalizar/' . $orderedItem->order_id, 'method' => 'post']) }}
-        {{ Form::submit('Finalizar pedido', ['class' => 'btn btn-primary']) }}
-    {{ Form::close() }}
+    <div class="text-center">
+        <h5>Valor total: R$ {{ $valorTotalCarrinho }}</h5>
+        {{ Form::open(['url' => 'carrinho/finalizar/' . $orderedItem->order_id, 'method' => 'post']) }}
+            {{ Form::submit('Finalizar pedido', ['class' => 'btn btn-primary']) }}
+        {{ Form::close() }}
+    </div>
 @else
     {{ 'O carrinho está vazio.' }}
 @endif
-</div>
 @endsection
