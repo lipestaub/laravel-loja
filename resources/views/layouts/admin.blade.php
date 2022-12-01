d1b0c69f182eb0364cc74416ae255d9f3c978e69
Felipe Staub <lipestaub@gmail.com>
2022-12-01 08:50:12 -0300

    style: adicionado menu dropdown

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -4,15 +4,34 @@
 <li>
     <a href="http://localhost:4200/carrinho">Carrinho</a>
 </li>
-<li>
-    <a href="http://localhost:4200/produtos/buscar">Buscar produtos</a>
-</li>
-<li>
-    <a href="http://localhost:4200/produtos/formulario">Cadastrar produtos</a>
-</li>
-<li>
-    <a href="http://localhost:4200/usuarios/formulario">Cadastrar Usu&aacute;rios</a>
-</li>
+
+<div class="dropdown">
+    <li>
+        <a href="http://localhost:4200/produtos/">Produtos</a>
+    </li>
+
+    <div class="dropdown-content">
+        <li>
+            <a href="http://localhost:4200/produtos/buscar">Buscar produtos</a>
+        </li>
+        <li>
+            <a href="http://localhost:4200/produtos/formulario">Cadastrar produtos</a>
+        </li>
+    </div>
+</div>
+
+<div class="dropdown">
+    <li>
+        <a href="http://localhost:4200/usuarios/">Usu&aacute;rios</a>
+    </li>
+
+    <div class="dropdown-content">
+        <li>
+            <a href="http://localhost:4200/usuarios/formulario">Cadastrar produtos</a>
+        </li>
+    </div>
+</div>
+
 <li>
     <a href="http://localhost:4200/produtos/controle-de-estoque">Controle de estoque</a>
 </li>
