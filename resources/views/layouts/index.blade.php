84312f40c49046da5c9c0f23e7feb61f973de629
Felipe Staub <lipestaub@gmail.com>
2022-12-08 09:14:13 -0300

    style: ajustar menu

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -13,45 +13,9 @@
     <title>Loja</title>
 </head>
 <body>
-    <div>
+    <div class="container">
             @if (empty(Auth::user()) || Auth::user()->user_type == 0)
-                <div class="col col-xs-12">
-                    <header class="py-3">
-                        <div class="container d-flex flex-wrap">
-                            <div class="col col-xs-1 mb-auto mt-auto">
-                                <a href="/" class="align-items-center text-dark text-decoration-none">
-                                    <span class="fs-4">Home</span>
-                                </a>
-                            </div>
-                            <div class="col col-xs-3 m-auto">
-                                {{ Form::open(['url' => 'produtos/buscar/resultados', 'method' => 'post']) }}
-                                    {{ Form::text('search', null, ['class' => 'form-control', 'placeholder' => 'Buscar', 'id' => 'campoBusca'])}}
-                                    <button class="btn btn-primary" type="submit"><i class="fa fa-search"></i></button>
-                                {{ Form::close() }}
-                            </div>
-                            <nav>
-                                <ul class="nav">
-                                    <div class="col col-xs-1 mb-auto mt-auto">
-                                        <div class="d-inline-flex">
-                                            <li>
-                                                <a href="http://localhost:4200/carrinho" class="nav-link link-dark px-2"><i class="fa fa-shopping-cart"></i> Carrinho</a>
-                                            </li>
-                                            <span class="p-2">Itens: {{ /*$totalItensCarrinho*/ 0}} R$ {{ /*$valorTotalCarrinho*/ 0 }}</span>
-                                        </div>
-                                        <div class="d-flex">
-                                            <li>
-                                                <a href="http://localhost:4200/minha-conta" class="nav-link link-dark px-2"><i class="fa fa-user"></i> Minha conta</a>
-                                            </li>
-                                            <li>
-                                                <a href="http://localhost:4200/meus-pedidos" class="nav-link link-dark px-2"><i class="fa fa-cubes"></i> Meus pedidos</a>
-                                            </li>
-                                        </div>
-                                    </div>
-                                </ul>
-                            </nav>
-                        </div>
-                    </header>
-                </div>
+                @include('layouts.loja')
             @elseif (Auth::user()->user_type == 0)
             @include('layouts.loja')
             @else
--- resources/views/layouts/loja.blade.php
+++ resources/views/layouts/loja.blade.php
@@ -1,15 +1,35 @@
-<li>
-    <a href="http://localhost:4200/carrinho" class="nav-link link-dark px-2"><i class="fa fa-shopping-cart"></i> Carrinho</a>
-</li>
-<li>
-    <a href="http://localhost:4200/minha-conta" class="nav-link link-dark px-2"><i class="fa fa-user"></i> Minha conta</a>
-</li>
-<li>
-    <a href="http://localhost:4200/meus-pedidos" class="nav-link link-dark px-2"><i class="fa fa-cubes"></i> Meus pedidos</a>
-</li>
-
-@if (Auth::user())
-    <li>
-        <a href="http://localhost:4200/logout" class="nav-link link-dark px-2">Sair</a>
-    </li>
-@endif
+<div class="text-center">
+    <header class="py-3 container d-flex flex-wrap text-center">
+        <div class="col-xs-4 mb-auto mt-auto">
+            <a href="/" class="align-items-center text-dark text-decoration-none">
+                <span class="fs-4">Home</span>
+            </a>
+        </div>
+        <div class="col-xs-4 m-auto">
+            {{ Form::open(['url' => 'produtos/buscar/resultados', 'method' => 'post']) }}
+                {{ Form::text('search', null, ['class' => 'form-control', 'placeholder' => 'Buscar', 'id' => 'campoBusca'])}}
+                <button class="btn btn-primary" type="submit"><i class="fa fa-search"></i></button>
+            {{ Form::close() }}
+        </div>
+        <nav class="col-xs-4">
+            <ul class="nav">
+                <div class="col-xs-1 mb-auto mt-auto">
+                    <div class="d-inline-flex">
+                        <li>
+                            <a href="/carrinho" class="nav-link link-dark px-2"><i class="fa fa-shopping-cart"></i> Carrinho</a>
+                        </li>
+                        <span class="p-2">Itens: {{ /*$totalItensCarrinho*/ 0}} R$ {{ /*$valorTotalCarrinho*/ 0 }}</span>
+                    </div>
+                    <div class="d-flex">
+                        <li>
+                            <a href="http://localhost:4200/minha-conta" class="nav-link link-dark px-2"><i class="fa fa-user"></i> Minha conta</a>
+                        </li>
+                        <li>
+                            <a href="/meus-pedidos" class="nav-link link-dark px-2"><i class="fa fa-cubes"></i> Meus pedidos</a>
+                        </li>
+                    </div>
+                </div>
+            </ul>
+        </nav>
+    </header>
+</div>
