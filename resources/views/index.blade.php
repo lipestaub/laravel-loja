cdbe8ce1705dba9a8671fdd195bc736295052cf7
Felipe Staub <lipestaub@gmail.com>
2022-12-12 11:50:03 -0300

    style: ajustar exibicao produtos da home

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -32,27 +32,32 @@
             @endforeach
         </div>
 
-        <div class="col-xs-12">
-            @foreach ($produtos as $produto)
-                <div class="col-xs-3 d-inline-block text-center p-2 border m-1">
-                    <div>
-                        @if ($produto->image_path == null)
-                            <img src="{{ asset('defaultProductImage.jpg') }}">
-                        @else
-                            <img class="" src="{{ asset($produto->image_path) }}">
-                        @endif
+        <div class="col-xs-12 mt-1 ms-5">
+            <div class="bg-primary">
+                <b><span class="text-white p-1"><i class="fa fa-chevron-right"></i> Destaques</span></b>
+            </div>
+            <div class="text-center mr-auto">
+                @foreach ($produtos as $produto)
+                    <div class="col-xs-3 d-inline-block text-center p-2 border m-1">
+                        <div>
+                            @if ($produto->image_path == null)
+                                <img src="{{ asset('defaultProductImage.jpg') }}">
+                            @else
+                                <img class="" src="{{ asset($produto->image_path) }}">
+                            @endif
+                        </div>
+                        <div>
+                            {{ $produto->description }}
+                        </div>
+                        <div>
+                            {{'R$ ' . $produto->price }}
+                        </div>
+                        <div>
+                            <a class="btn btn-primary" href="/produtos/comprar/{{ $produto->id }}">Comprar</a>
+                        </div>
                     </div>
-                    <div>
-                        {{ $produto->description }}
-                    </div>
-                    <div>
-                        {{'R$ ' . $produto->price }}
-                    </div>
-                    <div>
-                        <a class="btn btn-primary" href="/produtos/comprar/{{ $produto->id }}">Comprar</a>
-                    </div>
-                </div>
-            @endforeach
+                @endforeach
+            </div>
         </div>
     </div>
 @endsection
